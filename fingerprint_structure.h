0762c13aaf86acb0d45d85f9f7a4d47815a2b198
Michael Tjandra <miciltjandra@gmail.com>
2018-06-21 11:20:53 +0700

    Use CUDA_HOSTDEV

--- fingerprint_structure.h
+++ fingerprint_structure.h
@@ -1,6 +1,12 @@
 #ifndef FINGERPRINT_STRUCTURE_H
 #define FINGERPRINT_STRUCTURE_H
 
+#ifdef __CUDACC__
+#define CUDA_HOSTDEV __host__ __device__
+#else
+#define CUDA_HOSTDEV
+#endif
+
 #include <stdio.h>
 #include <vector>
 #include <string>
@@ -20,38 +26,38 @@ struct fingerprint {
     unsigned char avg_frequency; 
 };
 
-__host__ __device__ unsigned char orientation_to_byte(float orientation);
+CUDA_HOSTDEV unsigned char orientation_to_byte(float orientation);
 
-__host__ __device__ float byte_to_orientation(unsigned char c);
+CUDA_HOSTDEV float byte_to_orientation(unsigned char c);
 
-__host__ __device__ unsigned char coherence_to_byte(float coherence);
+CUDA_HOSTDEV unsigned char coherence_to_byte(float coherence);
 
-__host__ __device__ float byte_to_coherence(unsigned char c);
+CUDA_HOSTDEV float byte_to_coherence(unsigned char c);
 
-__host__ __device__ unsigned char frequency_to_byte(float frequency);
+CUDA_HOSTDEV unsigned char frequency_to_byte(float frequency);
 
-__host__ __device__ float byte_to_frequency(unsigned char c);
+CUDA_HOSTDEV float byte_to_frequency(unsigned char c);
 
-__host__ __device__ unsigned char period_to_byte(float period);
+CUDA_HOSTDEV unsigned char period_to_byte(float period);
 
-__host__ __device__ float byte_to_period(unsigned char c);
+CUDA_HOSTDEV float byte_to_period(unsigned char c);
 
-__host__ __device__ struct fingerprint make_fingerprint_struct(int id, std::vector<float> local_orientation, std::vector<float> local_coherence, std::vector<float> local_frequency, float avg_orie, float avg_freq);
+CUDA_HOSTDEV struct fingerprint make_fingerprint_struct(int id, std::vector<float> local_orientation, std::vector<float> local_coherence, std::vector<float> local_frequency, float avg_orie, float avg_freq);
 
-__host__ __device__ void print_fingerprint_struct(struct fingerprint fp);
+CUDA_HOSTDEV void print_fingerprint_struct(struct fingerprint fp);
 
-__host__ __device__ void get_fingerprint_local_values(struct fingerprint fp, std::vector<float> &local_orientation, std::vector<float> &local_coherence, std::vector<float> &local_frequency);
+CUDA_HOSTDEV void get_fingerprint_local_values(struct fingerprint fp, std::vector<float> &local_orientation, std::vector<float> &local_coherence, std::vector<float> &local_frequency);
 
-__host__ __device__ float get_fingerprint_average_orientation(struct fingerprint fp);
+CUDA_HOSTDEV float get_fingerprint_average_orientation(struct fingerprint fp);
 
-__host__ __device__ float get_fingerprint_average_frequency(struct fingerprint fp);
+CUDA_HOSTDEV float get_fingerprint_average_frequency(struct fingerprint fp);
 
-__host__ __device__ void save_to_file(int size, struct fingerprint fps[], std::string filename);
+CUDA_HOSTDEV void save_to_file(int size, struct fingerprint fps[], std::string filename);
 
-__host__ __device__ int read_from_file(std::vector<struct fingerprint> &fps, std::string filename);
+CUDA_HOSTDEV int read_from_file(std::vector<struct fingerprint> &fps, std::string filename);
 
-__host__ __device__ int get_last_id_from_file(std::string filename);
+CUDA_HOSTDEV int get_last_id_from_file(std::string filename);
 
-__host__ __device__ int get_new_fingerprint_id(int last_id);
+CUDA_HOSTDEV int get_new_fingerprint_id(int last_id);
 
 #endif
